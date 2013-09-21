65afc8adc55380c71d60cf4b90f6793e8fd55ebd
Daniele <daniele.lenares@gmail.com>
2013-09-21 10:23:36 +0200

    Fixed some errors + Added specific Ruby files

--- main.js
+++ main.js
@@ -2,20 +2,28 @@
 /*global define, $, brackets, window */
 
 define(function (require, exports, module) {
-    'use strict';
-    
-    var LanguageManager = brackets.getModule("language/LanguageManager");
+	'use strict';
 
-    //Ruby Refine
-    var ruby_language = LanguageManager.getLanguage("ruby");
-    ruby_language.addFileExtension("rb");
-    ruby_language.addFileName("Ruby");
-    ruby_language.setLineCommentSyntax(["#"]);
-    ruby_language.setBlockCommentSyntax("=begin\n", "\n=end");
-    
-    //Erb Refine
-    var erb_language = LanguageManager.getLanguage("html");
-    erb_language.addFileExtension("html.erb");
-    erb_language.addFileName("ERB");
-    erb_language.setBlockCommentSyntax("<!--", "-->");    
+	var LanguageManager = brackets.getModule("language/LanguageManager");
+
+	//Ruby Refine
+	var ruby_language = LanguageManager.getLanguage("ruby");
+	//RoR Extensions
+	ruby_language.addFileExtension("rb");
+	ruby_language.addFileExtension("ru");
+	//RoR Specific Files
+	ruby_language.addFileName("Gemfile");
+	ruby_language.addFileName("Rakefile");
+	//Ruby Comments
+	ruby_language.setLineCommentSyntax(["#"]);
+	ruby_language.setBlockCommentSyntax("=begin\n", "\n=end");
+
+	//ERB Refine
+	var erb_language = LanguageManager.getLanguage("html");
+	//ERB Extensions
+	erb_language.addFileExtension("html.erb");
+	erb_language.addFileExtension("htm.erb");
+	erb_language.addFileExtension("erb");
+	//ERB Comments
+	erb_language.setBlockCommentSyntax("<!--", "-->");
 });
